4368793ab6a0417deb16f6a6db73788eb4367b6f
Siddhanth Lalgowdar <61350672+siddhanth78@users.noreply.github.com>
2024-08-19 01:59:53 -0600

    Update im_to_ascii.py

--- im_to_ascii.py
+++ im_to_ascii.py
@@ -10,8 +10,8 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 new_width = 600
 
-aspect_ratio = h / w
-new_height = int(new_width * aspect_ratio)
+aspect_ratio = w / h
+new_height = int(new_width/aspect_ratio)
 
 resized_image = cv2.resize(image, (new_width, new_height))
 
